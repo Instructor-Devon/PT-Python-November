8fe246d32f7c8b664fbab7867a8f53f3b9a880df
Devon <=>
2019-11-12 18:56:56 -0800

    sql alchemy many-to-manys

---
+++ 6Week/1Day/POSTerized/app.py
@@ -0,0 +1,221 @@
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from validations.validators import validate
+import re
+# from app import User
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+app = Flask(__name__)
+# configurations to tell our app about the database we'll be connecting to
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_dash.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "LOL"
+
+bcrypt = Bcrypt(app)
+
+# an instance of the ORM
+db = SQLAlchemy(app)
+# a tool for allowing migrations/creation of tables
+migrate = Migrate(app, db)
+
+
+#NEW!
+likes_table = db.Table('likes', 
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id', ondelete="cascade"), primary_key=True), 
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id', ondelete="cascade"), primary_key=True))
+
+# some_user.posts => [Post, Post]
+class User(db.Model):	
+    __tablename__ = "users"    # optional		
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(45))
+    last_name = db.Column(db.String(45))
+    email = db.Column(db.String(45), unique=True)
+    password = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+
+    #NEW!
+    likes_sent = db.relationship("Post", secondary=likes_table)
+    #posts
+
+    def __repr__(self):
+        return f"<User: {self.email}>"
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="cascade"), nullable=False)
+    author = db.relationship('User', foreign_keys=[author_id], backref="posts")
+
+    created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+
+    #NEW! [User1, User2, User3]
+    likes_rec = db.relationship("User", secondary=likes_table)
+
+    @property
+    def num_likes(self):
+        # likes_rec [User1, User2]
+        return len(self.likes_rec)
+
+    def __repr__(self):
+        return f"<Post: \"{self.content[:5]}...\">"
+
+@app.route("/")
+def index():
+    return render_template("index.html", users=User.query.all())
+
+@app.route("/users/create", methods=["POST"])
+def register():
+    errors = []
+
+    if len(request.form['first_name']) < 2:
+        errors.append("First name must be at least 2 characters")
+        valid = False
+
+    if len(request.form['last_name']) < 2:
+        errors.append("Last name must be at least 2 characters")
+        valid = False
+
+    if not EMAIL_REGEX.match(request.form['email']):
+        errors.append("Email must be valid")
+        valid = False
+
+    if len(request.form['password']) < 8:
+        errors.append("Password must be at least 8 characters")
+        valid = False
+
+    #TODO: Validate email is unique
+    user_check = User.query.filter_by(email=request.form["email"]).first()
+    if user_check is not None:
+        errors.append("Email is in use")
+    
+    if request.form['password'] != request.form['confirm']:
+        errors.append("Passwords must match")
+        valid = False
+
+    if errors:
+        for e in errors:
+            flash(e)
+    else:
+        hashed = bcrypt.generate_password_hash(request.form["password"])
+        new_user = None
+        #TODO: Create New User
+        new_user = User(
+            first_name = request.form["first_name"],
+            last_name = request.form["last_name"],
+            email = request.form["email"],
+            password = hashed
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        session["user_id"] = new_user.id
+        return redirect("/dashboard")
+
+    return redirect("/")
+
+@app.route("/users/login", methods=["POST"])
+def login():
+    errors = []
+
+    user_attempt = User.query.filter_by(email=request.form["email"]).first()
+    #TODO: Query for user wiith provided email
+    
+    if not user_attempt:
+        flash("Email/Password Incorrect")
+        return redirect("/")
+
+    if not bcrypt.check_password_hash(user_attempt.password, request.form["password"]):
+        flash("Email/Password Incorrect")
+        return redirect("/")
+
+    session["user_id"] = user_attempt.id
+    return redirect('/dashboard')
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/dashboard")
+def dashboard():
+    # get user from session
+    if not "user_id" in session:
+        return redirect("/")
+    
+    logged_in = User.query.get(session["user_id"])
+    #TODO: Query for user with session id
+
+    all_posts = Post.query.all()
+    #TODO: Query for all posts, ordered by the most recent
+
+
+    return render_template("dashboard.html", user=logged_in, posts=all_posts)
+
+@app.route("/posts/create", methods=["POST"])
+def new_post():
+    if not "user_id" in session:
+        return redirect("/")
+    
+
+    new_post = Post(content=request.form["content"], author_id=session["user_id"])
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect("/dashboard")
+
+@app.route("/posts/<post_id>/show")
+def show_post(post_id):
+
+    # query for a post with id
+    post = Post.query.get(post_id)
+    return render_template("post_details.html", post=post)
+
+@app.route("/posts/<post_id>/update", methods=["POST"])
+def update_post(post_id):
+
+    # query for a post with id
+    post = Post.query.get(post_id)
+
+    # update with new stuff!
+    post.content = request.form["content"]
+
+    db.session.commit()
+
+    return redirect("/dashboard")
+
+@app.route("/posts/<post_id>/delete")
+def delete_post(post_id):
+
+    # query for a post with id
+    post = Post.query.get(post_id)
+
+    # delete from db.session
+    db.session.delete(post)
+
+    # commit to db
+    db.session.commit()
+
+    return redirect("/dashboard")
+
+@app.route("/likes/<post_id>")
+def add_like(post_id):
+    # query for a user (that's doing the liking)
+    logged_in_user_id = session["user_id"]
+    user = User.query.get(logged_in_user_id)
+
+    # query for a post!
+    post = Post.query.get(post_id)
+
+    user.likes_sent.append(post)
+    db.session.commit()
+
+    return redirect("/dashboard")
+
+if __name__ == "__main__":
+    app.run(debug=True)
